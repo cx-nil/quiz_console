1a2037b717ce44841ee180a3b995c2bc5691c5c8
cx-nil <cx060@codelogicx.com>
2026-01-02 11:58:11 +0530

    (feat): displaying answers after playing quiz

--- QuestionService.java
+++ QuestionService.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class QuestionService {
   Question[] questions = new Question[5];
+  String selection[] = new String[5];
 
   public QuestionService() {
     questions[0] = new Question(1, "size of int", "2", "6", "4", "8", "4");
@@ -9,11 +12,26 @@ public class QuestionService {
     questions[4] = new Question(5, "size of boolean", "1", "2", "4", "8", "1");
   }
 
-  public void displayQuestions() {
+  public void playQuiz() {
 
+    int i = 0;
     for(Question q : questions) {
 
-      System.out.println(q.toString());
+      System.out.println("Question no: " + q.getId());
+      System.out.println(q.getQuestion());
+
+      System.out.println(q.getOpt1());
+      System.out.println(q.getOpt2());
+      System.out.println(q.getOpt3());
+      System.out.println(q.getOpt4());
+
+      Scanner sc = new Scanner(System.in);
+      selection[i] = sc.nextLine();
+      i++;
+    }
+
+    for(String s: selection) {
+      System.out.println(s);
     }
 
   }
